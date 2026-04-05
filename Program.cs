894f19001b77e21dfbfb6c5de56e8600b9920a2c
n3r3v1 <annakurochkina@icloud.com>
2026-04-05 21:32:01 +0300

    commit 1 try 1

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class PetStoreItem
+{
+
+    private static int count = 0;
+    private readonly int itemCode;
+    private string name;
+    private double price;
+
+    public int ItemCode { get { return itemCode; } }
+    public string Name { get { return name; } }
+    public double Price { get { return price; } }
+
+    public PetStoreItem(int itemCode, string name, double price)
+    {
+        this.itemCode = itemCode;
+        this.name = name;
+        this.price = price;
+        count++;
+    }
+
+    public static int GetTotalCount()
+    {
+        return count;
+    }
+
+    public virtual string GetInfo()
+    {
+        return $"Код: {itemCode}, Назва: '{name}', Ціна: {price} грн";
+    }
+
+    public bool IsExpensive()
+    {
+        return price > 800;
+    }
+
+}
+
+public class Food : PetStoreItem
+{
+    private string animalType;
+    public Food(int itemCode, string name, double price, string animalType)
+              : base(itemCode, name, price)
+    {
+        this.animalType = animalType;
+    }
+
+}
+public class Accessory : PetStoreItem
+{
+    private string material;
+
+    public Accessory(int itemCode, string name, double price, string material)
+        : base(itemCode, name, price)
+    {
+        this.material = material;
+    }
+
+    public override string GetInfo()
+    {
+        return base.GetInfo() + $" | Матеріал: {material} (Аксесуар)";
+    }
+}
+
+
+
